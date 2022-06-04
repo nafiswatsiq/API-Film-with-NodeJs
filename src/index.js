31ea8bea7c30a7796c424f2ebf96c3a2a694215d
nafiswatsiq <amrullohnafis@gmail.com>
2022-06-05 04:08:30 +0700

    input tags and query tags

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const slugify = require('slugify');
 const app = express();
 const { db } = require('./model/conection');
+const randomstring = require("randomstring");
 
 app.use(cors());
 app.use(express.json());
@@ -11,8 +12,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 //read
 app.get('/api/list-film', (req, res) => {
-    const sqlQuery = "SELECT * FROM `film` WHERE 1";
-
+    const sqlQuery = "SELECT * FROM film";
+    
     db.query(sqlQuery, (err, result) =>{
         if(err){
             console.log(err);
@@ -23,6 +24,39 @@ app.get('/api/list-film', (req, res) => {
     })
 });
 
+// app.get('/api/list-film', (req, res) => {
+//     const sqlQuery = "SELECT * FROM `film`";
+    
+//     db.query(sqlQuery, (err, result) =>{
+//         if(err){
+//             console.log(err);
+//         } else{
+//             // res.send(result)
+//             let result_ = [];
+//             for(let i = 0; i < result.length; i++){
+//                 result_.push({
+//                     title: result[i].title,
+//                     slug: result[i].slug,
+//                     description: result[i].description,
+//                     thumbnail: result[i].thumbnail,
+//                     link_film: result[i].link_film,
+//                     rating: result[i].rating,
+//                     type: result[i].type,
+//                     tgl_upload: result[i].tgl_upload,
+//                     // tags: resultTags
+//                 })
+//             };
+
+//             return res.status(200).json({
+//                 status: true,
+//                 message: 'List Data Posts',
+//                 data: result_,
+//             })
+//         }
+//     })
+// });
+
+
 app.get('/api/list-film/:slug', (req, res) => {
     const getFilm = req.params.slug;
 
@@ -31,8 +65,27 @@ app.get('/api/list-film/:slug', (req, res) => {
         if(err){
             console.log(err);
         } else{
-            res.send(result)
-            console.log(result)
+            // res.send(result)
+            const sqlQueryTags = "SELECT tags.id,tags.id_film,tags.tags FROM film JOIN tags ON film.id_film = tags.id_film WHERE film.id_film = ?;";
+            db.query(sqlQueryTags, result[0].id_film, (err, resultTags) =>{
+                
+                let result_ = [];
+                for(let i = 0; i < result.length; i++){
+                    result_.push({
+                        id_film: result[i].id_film,
+                        title: result[i].title,
+                        slug: result[i].slug,
+                        description: result[i].description,
+                        thumbnail: result[i].thumbnail,
+                        link_film: result[i].link_film,
+                        rating: result[i].rating,
+                        type: result[i].type,
+                        tgl_upload: result[i].tgl_upload,
+                        tags: resultTags
+                    })
+                };
+                return res.status(200).json(result_)
+            })
         }
     })
 })
@@ -48,17 +101,19 @@ app.post('/api/add-film', (req, res) =>{
         locale: 'en',
         trim: true,
     }
+    const id_film = randomstring.generate(25);
     const slug = slugify(req.body.title, options)
     const title = req.body.title,
         description = req.body.description,
         thumbnail = req.body.thumbnail,
         link_film = req.body.link_film,
         rating = req.body.rating,
-        id_tags = req.body.id_tags;
+        type = req.body.type,
+        tags = req.body.tags;
 
-    const sqlQuery = "INSERT INTO `film`(`title`, `slug`, `description`, `thumbnail`, `link_film`, `rating`, `id_tags`) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    const sqlQuery = "INSERT INTO `film`(`id_film`, `title`, `slug`, `description`, `thumbnail`, `link_film`, `rating`, `type`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
     
-    db.query(sqlQuery, [title, slug, description,thumbnail,link_film,rating,id_tags], (err, result) => {
+    db.query(sqlQuery, [id_film ,title, slug, description,thumbnail,link_film,rating,type], (err, result) => {
         if(err){
             console.log(err);
         } else{
@@ -66,28 +121,68 @@ app.post('/api/add-film', (req, res) =>{
             console.log(result)
         }
     })
+
+    for(let i = 0; i < tags.length; i++){
+        const sqlQueryTags = "INSERT INTO `tags`(`id_film`, `tags`) VALUES (?, ?)";
+        db.query(sqlQueryTags, [id_film, tags[i].tags], (err, result) => {
+            if(err){
+                console.log(err);
+            } else{
+                // res.send(result)
+                console.log(result)
+            }
+        })
+    }
 })
 
 //update
-app.put('/api/update-film/:id', (req, res) => {
-    const id = req.params.id;
+app.put('/api/update-film/:id_film', (req, res) => {
+    const id_film = req.params.id_film;
     // const id = req.body.id;
-    const slug = slugify(req.body.title)
+    const options = {
+        replacement: '-',
+        remove: undefined,
+        lower: true,
+        strict: false,
+        locale: 'en',
+        trim: true,
+    }
+    const slug = slugify(req.body.title, options)
     const title = req.body.title,
         description = req.body.description,
         thumbnail = req.body.thumbnail,
         link_film = req.body.link_film,
         rating = req.body.rating,
-        id_tags = req.body.id_tags;
+        type = req.body.type,
+        tags = req.body.tags;
     
-    const sqlQuery = "UPDATE `film` SET `title`= ?,`slug`= ?,`description`= ?,`thumbnail`= ?,`link_film`= ?,`rating`= ?,`id_tags`= ? WHERE id =?"
+    const sqlQuery = "UPDATE `film` SET `title`= ?,`slug`= ?,`description`= ?,`thumbnail`= ?,`link_film`= ?,`rating`= ?,`type`= ? WHERE id_film =?"
 
-    db.query(sqlQuery, [title, slug, description, thumbnail, link_film, rating, id_tags, id], (err, result) => {
+    db.query(sqlQuery, [title, slug, description, thumbnail, link_film, rating, type, id_film], (err, result) => {
         if(err){
             console.log(err);
         } else{
             res.send(result)
-            console.log(result)
+
+            const sqlQueryTags = "DELETE FROM `tags` WHERE id_film = ?";
+            db.query(sqlQueryTags, id_film, (err, result) => {
+                if(err){
+                    console.log(err);
+                } else{
+                    // console.log(result)
+                    for(let i = 0; i < tags.length; i++){
+                        const sqlQueryTags = "INSERT INTO `tags`(`id_film`, `tags`) VALUES (?, ?)";
+                        db.query(sqlQueryTags, [id_film, tags[i].tags], (err, result) => {
+                            if(err){
+                                console.log(err);
+                            } else{
+                                console.log(result)
+                            }
+                        })
+                    }
+                }
+            })
+            // console.log(result)
         }
     })
 })
